72e2ab21819727abe79807715b5693bf54c4959d
taksapoy <124069630+taksapoy@users.noreply.github.com>
2023-02-08 12:43:57 +0700

    Update bfs.ts

--- bfs.ts
+++ bfs.ts
@@ -110,36 +110,7 @@ class BinarySearchTree {
         return null
     }
 
-    breadthFirstSearch() {
-        if (this.root === null)
-            return null
-        let currentNode = this.root
-        let bfs = []
-        let queue = []
-        queue.push(currentNode)
-        while (queue.length > 0) {
-            currentNode = queue.shift()!
-            bfs.push(currentNode.value)
-            if (currentNode.left !== null)
-                queue.push(currentNode.left)
-            if (currentNode.right !== null)
-                queue.push(currentNode.right)
-        }
-        return bfs
-    }
-    breadthFirstSearchRecusive(queue: any[] = [this.root], bfs: any[] = []): any[] | null {
-        if (queue.length < 1)
-            return bfs
-        const currentNode = queue.shift()
-        bfs.push(currentNode.value)
-        if (currentNode.left !== null)
-            queue.push(currentNode.left)
-        if (currentNode.right !== null)
-            queue.push(currentNode.right)
-        return this.breadthFirstSearchRecusive(queue, bfs)
-    }
-}
-
+    
 function breadthFirstSearch(tree: BinarySearchTree) {
     function traverse(queue: any[], bfs: any[]): any[] | null {
         if (queue.length < 1)
